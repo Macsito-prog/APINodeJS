2089379cdc03c6f939f7c0daaddd3de6c5d620b4
Maximiliano Carrasco <max.carrascoc@duocuc.cl>
2023-06-19 16:41:55 -0400

    se finalizó la api para ordenes de compra

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const sql = require('msnodesqlv8');
+const sql = require("msnodesqlv8");
 
 const bdconnect = {
-  database: 'gestionComercial',
-  server: 'DESKTOP-FDUSN85\\SQLEXPRESS',
-  driver: 'msnodesqlv8',
+  database: "gestionComercial",
+  server: "DESKTOP-FDUSN85\\SQLEXPRESS",
+  driver: "msnodesqlv8",
   options: {
-    trustedConnection: true
-  }
+    trustedConnection: true,
+  },
 };
 
-const connectionString = `Driver={SQL Server Native Client 11.0};Server=${bdconnect.server};Database=${bdconnect.database};Trusted_Connection=${bdconnect.options.trustedConnection ? 'Yes' : 'No'};`;
+const connectionString = `Driver={SQL Server Native Client 11.0};Server=${
+  bdconnect.server
+};Database=${bdconnect.database};Trusted_Connection=${
+  bdconnect.options.trustedConnection ? "Yes" : "No"
+};`;
 
-
-app.use(express.json())
+app.use(express.json());
 
 //ruta para obtener las ordenes de compra
 
-app.get('/datos', (req, res) => {
+app.get("/datos", (req, res) => {
   const rutProveedor = req.query.rutProveedor;
 
   if (!rutProveedor) {
@@ -28,7 +31,7 @@ app.get('/datos', (req, res) => {
   sql.open(connectionString, (err, conn) => {
     if (err) {
       console.error(err);
-      return res.status(500).send('Error al conectar con la base de datos');
+      return res.status(500).send("Error al conectar con la base de datos");
     }
 
     const query = `SELECT * FROM ORDEN_COMPRA WHERE RUT_PROVEEDOR = '${rutProveedor}'`;
@@ -36,20 +39,20 @@ app.get('/datos', (req, res) => {
     conn.query(query, (err, rows) => {
       if (err) {
         console.error(err);
-        return res.status(500).send('Error al ejecutar la consulta');
+        return res.status(500).send("Error al ejecutar la consulta");
       }
 
       // Envía los resultados como respuesta al navegador
       res.json(rows);
-      
+
       // Cierra la conexión después de usarla
       conn.close();
     });
   });
 });
 
-//ruta para obtener los detalles de la orden de compra.
-app.get('/orden-compra/:idOrdenCompra/detalle', (req, res) => {
+//ruta para obtener los detalles de la orden de compra según ID
+app.get("/orden-compra/:idOrdenCompra/detalle", (req, res) => {
   const idOrdenCompra = req.params.idOrdenCompra;
 
   if (!idOrdenCompra) {
@@ -76,10 +79,73 @@ app.get('/orden-compra/:idOrdenCompra/detalle', (req, res) => {
     });
   });
 });
+
+//obtener el detalle de todas las ordenes de compra.
+
+app.get("/detalle-orden-compra", (req, res) => {
+  sql.open(connectionString, (err, conn) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send("Error al conectar con la base de datos");
+    }
+
+    const query = `
+      SELECT DOC.*, OC.RUT_PROVEEDOR
+      FROM DETALLE_ORDEN_COMPRA DOC
+      INNER JOIN ORDEN_COMPRA OC ON DOC.ID_ORDEN_COMPRA = OC.ID_ORDEN_COMPRA
+    `;
+
+    conn.query(query, (err, rows) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).send("Error al ejecutar la consulta");
+      }
+
+      res.json(rows);
+
+      conn.close();
+    });
+  });
+});
+
+//obtener orden_compra según rut
+app.get("/proveedores/:rutProveedor/ordenes-compra", (req, res) => {
+  const rutProveedor = req.params.rutProveedor;
+
+  if (!rutProveedor) {
+    return res.status(400).send('El parámetro "rutProveedor" es obligatorio');
+  }
+
+  sql.open(connectionString, (err, conn) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send("Error al conectar con la base de datos");
+    }
+
+    const query = `
+      SELECT OC.ID_ORDEN_COMPRA, OC.RUT_PROVEEDOR, DOC.ID_DETALLE_ORDEN, DOC.ID_PRODUCTO, DOC.CANTIDAD
+      FROM ORDEN_COMPRA OC
+      INNER JOIN DETALLE_ORDEN_COMPRA DOC ON OC.ID_ORDEN_COMPRA = DOC.ID_ORDEN_COMPRA
+      WHERE OC.RUT_PROVEEDOR = '${rutProveedor}'
+    `;
+
+    conn.query(query, (err, rows) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).send("Error al ejecutar la consulta");
+      }
+
+      res.json(rows);
+
+      conn.close();
+    });
+  });
+});
+
 //RUTAS POST
 
 //generar el detalle de una orden de compra
-app.post('/orden-compra/:idOrdenCompra/detalle', (req, res) => {
+app.post("/orden-compra/:idOrdenCompra/detalle", (req, res) => {
   const idOrdenCompra = req.params.idOrdenCompra;
   const { ID_PRODUCTO, CANTIDAD } = req.body;
 
@@ -87,36 +153,61 @@ app.post('/orden-compra/:idOrdenCompra/detalle', (req, res) => {
     return res.status(400).send("Todos los campos son obligatorios");
   }
 
-  sql.open(connectionString, (err,conn)=>{
-    if(err){
+  sql.open(connectionString, (err, conn) => {
+    if (err) {
       console.error(err);
       return res.status(500).send("Error al conectar con la base de datos");
     }
 
     const query = `INSERT INTO DETALLE_ORDEN_COMPRA (ID_ORDEN_COMPRA, ID_PRODUCTO, CANTIDAD) VALUES (${idOrdenCompra}, ${ID_PRODUCTO}, ${CANTIDAD})`;
 
-    conn.query(query, (err, rows)=>{
-      if(err){
+    conn.query(query, (err, rows) => {
+      if (err) {
         console.error(err);
-        return res.status(500).send("Error al ejecutar la consult.")
+        return res.status(500).send("Error al ejecutar la consult.");
       }
 
-      res.json({ message: "Detalle de la orden de compra agregado correctamente" });
-
+      res.json({
+        message: "Detalle de la orden de compra agregado correctamente",
+      });
 
       conn.close();
-    })
+    });
 
-  })
+    // respuesta exitosa
+    res.status(200).send("Orden de compra generada correctamente");
+  });
+});
 
+app.post("/ordenes-compra", (req, res) => {
+  const rutProveedor = req.body.rutProveedor;
 
+  if (!rutProveedor) {
+    return res.status(400).send('El parámetro "rutProveedor" es obligatorio');
+  }
+
+  sql.open(connectionString, (err, conn) => {
+    if (err) {
+      console.error("error al conectar con la base de datos");
+    }
+
+    const query = `INSERT INTO ORDEN_COMPRA (RUT_PROVEEDOR) VALUES ('${rutProveedor}')`;
+
+    conn.query(query, (err, rows) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).send("Error al ejecutar la consult.");
+      }
+
+      res.json({ message: "Orden de compra agregada correctamente" });
+
+      conn.close();
+    });
+  });
 });
 
 
 
-
-
-
 app.listen(8080, () => {
-  console.log('Servidor: http://localhost:8080/datos');
+  console.log("Servidor: http://localhost:8080/datos");
 });
